4fd4ec171b3c425432fa16195bed6d196e291fd1
ahnella04 <chqkq96@gmail.com>
2021-09-09 11:32:54 +0900

    react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import './App.css';
 import Login from "./screens/Login";
 
 const App: React.FC = () => {
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,15 +1,50 @@
 import React from "react";
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
 
 const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
     height: 100vh;
     background-color: #EFF1FF;
+`;
+
+const AuthBox = styled.div`
+    width: 350px;
+    height: 500px;
+    border-radius: 15px;
+    box-shadow: 5px 5px 20px #36359E;
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 25px;
+    font-weight: 700;
+    color: #3736D4;
+    margin-top: 15px;
+`;
+
+const Input = styled.input`
+    width: 100%;
+    border-radius: 3px;
+    padding: 5px;
+    background-color: #fafafa;
+    
 `;
 
 const Login: React.FC = () => {
     return (
         <Container>
-            
+            <AuthBox>
+                <Wrapper>Login</Wrapper>
+                <form>
+                    <Input />
+                </form>
+            </AuthBox>
         </Container>
     )
 }
